9925865e0229d6a2c2ce54d5fdd622ea49688831
Adam Atkinson <adatkins@microsoft.com>
2024-12-16 10:22:03 -0800

    resolve flash attn issue with a hack

---
+++ benchmarks/inference/mii/install_MOD_custom_vllm_yocov2_bic-image.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# source me!
+conda create -n myenv2 python=3.10 -y
+## If conda activate fails, then need to run this command 
+source activate myenv2
+conda activate myenv2
+## verify that the correct env is used by
+conda env list
+
+# git clone & checkout https://github.com/microsoft/vllm/tree/congcongchen/yoco_v2 at commit 61494be7d7e4ec447369a2a82565c753565eb310
+cd /data/users/adatkins/dev/phivnext/yoco/yocov2/vllm
+
+conda install -y ccache
+export MAX_JOBS=8192 # set this to larger num to make build faster
+export NVCC_THREADS=128
+
+# # opt 1
+# pip install flash-attn==2.4.2 # --force-reinstall should install torch?
+
+pip install --user -e . -vvv # This may take 5-10 minutes.
+
+# for Amit's benchmark
+pip install matplotlib deepspeed-mii>=0.2.0 tabulate
+
+# opt 2
+pip install uninstall flash-attn
+pip install -U flash-attn
+# pip install flash-attn==2.4.2 # fails
+
+cd /data/users/adatkins/dev/phivnext/yoco/yocov2/DeepSpeedExamples/benchmarks/inference/mii
