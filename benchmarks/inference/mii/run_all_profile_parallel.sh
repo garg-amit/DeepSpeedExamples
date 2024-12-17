9c2ec548d1183c99c8f0abe8722d970eb9983afa
Adam Atkinson <adatkins@microsoft.com>
2024-12-17 12:40:34 -0800

    more parallel

--- benchmarks/inference/mii/run_all_profile_parallel.sh
+++ benchmarks/inference/mii/run_all_profile_parallel.sh
@@ -6,8 +6,9 @@
 LOAD_FORMAT=dummy
 BACKEND="vllm"
 
-OUT_DIR=./results_profile_yocov2_without_DA_cudagraph_ON
-MODEL="/data/users/adatkins/dev/phivnext/yoco/yocov2/DeepSpeedExamples/benchmarks/inference/mii/dummy_checkpoints/yocov2_samba_no_da_hf"
+OUT_DIR=./results_profile_llama32-3B_without_DA_cudagraph_ON
+MODEL="meta-llama/Llama-3.2-3B"
+
 
 python ./run_benchmark.py --backend ${BACKEND}  --model ${MODEL} --mean_prompt_length 500 --mean_max_new_tokens 500 --tp_size 1 --out_json_dir ${OUT_DIR} --load_format ${LOAD_FORMAT} --stream --cuda_visible_devices 0 --port 26500 --overwrite_results --parallel &
 
@@ -15,17 +16,15 @@ python ./run_benchmark.py --backend ${BACKEND}  --model ${MODEL} --mean_prompt_l
 
 python ./run_benchmark.py --backend ${BACKEND}  --model ${MODEL} --mean_prompt_length 4000 --mean_max_new_tokens 500 --tp_size 1 --out_json_dir ${OUT_DIR} --load_format ${LOAD_FORMAT} --stream --cuda_visible_devices 2 --port 26502 --overwrite_results --parallel &
 
-# error with this one?
-python ./run_benchmark.py --backend ${BACKEND}  --model ${MODEL} --mean_prompt_length 3500 --mean_max_new_tokens 3500 --tp_size 1 --out_json_dir ${OUT_DIR} --load_format ${LOAD_FORMAT} --stream --cuda_visible_devices 3 --port 26503 --overwrite_results --parallel &
+# # error with this one?
+# python ./run_benchmark.py --backend ${BACKEND}  --model ${MODEL} --mean_prompt_length 3500 --mean_max_new_tokens 3500 --tp_size 1 --out_json_dir ${OUT_DIR} --load_format ${LOAD_FORMAT} --stream --cuda_visible_devices 3 --port 26503 --overwrite_results --parallel &
 
-OUT_DIR=./results_profile_yocov2_without_DA_cudagraph_OFF
+OUT_DIR=./results_profile_llama32-3B_without_DA_cudagraph_OFF
 python ./run_benchmark.py --backend ${BACKEND}  --model ${MODEL} --mean_prompt_length 500 --mean_max_new_tokens 500 --tp_size 1 --out_json_dir ${OUT_DIR} --load_format ${LOAD_FORMAT} --stream --cuda_visible_devices 4 --port 26504 --overwrite_results --parallel --enforce_eager &
 
 python ./run_benchmark.py --backend ${BACKEND}  --model ${MODEL} --mean_prompt_length 500 --mean_max_new_tokens 4000 --tp_size 1 --out_json_dir ${OUT_DIR} --load_format ${LOAD_FORMAT} --stream --cuda_visible_devices 5 --port 26505 --overwrite_results --parallel --enforce_eager &
 
 python ./run_benchmark.py --backend ${BACKEND}  --model ${MODEL} --mean_prompt_length 4000 --mean_max_new_tokens 500 --tp_size 1 --out_json_dir ${OUT_DIR} --load_format ${LOAD_FORMAT} --stream --cuda_visible_devices 6 --port 26506 --overwrite_results --parallel --enforce_eager &
 
-# watch for error
-python ./run_benchmark.py --backend ${BACKEND}  --model ${MODEL} --mean_prompt_length 3500 --mean_max_new_tokens 3500 --tp_size 1 --out_json_dir ${OUT_DIR} --load_format ${LOAD_FORMAT} --stream --cuda_visible_devices 7 --port 26507 --overwrite_results --parallel --enforce_eager &
-
-# TODO # MODEL="meta-llama/Llama-3.2-3B"
+# # watch for error
+# python ./run_benchmark.py --backend ${BACKEND}  --model ${MODEL} --mean_prompt_length 3500 --mean_max_new_tokens 3500 --tp_size 1 --out_json_dir ${OUT_DIR} --load_format ${LOAD_FORMAT} --stream --cuda_visible_devices 7 --port 26507 --overwrite_results --parallel --enforce_eager &
